6476209cc1b06bcd40eba995b7ac19687a514c19
david00721 <david007214@gmail.com>
2021-11-06 14:11:08 +0100

    edited file names, added documentation, finished work until consultation

--- main.py
+++ main.py
@@ -2,71 +2,144 @@ import cv2
 import numpy as np 
 import imutils
 import pytesseract
-import string
-#import easyocr
 from matplotlib import pyplot as plt
+from difflib import SequenceMatcher
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+def similar(a, b):
+    if b is not None:
+        return SequenceMatcher(None, a, b).ratio()
+    else:
+        return 0.0
+
+def get_knownplate(inputpath):
+    inputpath = inputpath.replace("rendszam kepek\\\\", "")
+    inputpath = inputpath.replace(".jpg", "")
+    return inputpath
+
+def order_points(pts):
+	rect = np.zeros((4, 2), dtype = "float32")
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+	return rect
+
+def four_point_transform(image, pts):
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+	dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	return warped
 
 #----------rendszám kinyerő függvény-----------
 
-def numberplate_to_text(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    
-    cv2.imshow("Kiindulo kep", img)
-    cv2.imshow("Szurke kep", gray)
-    bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #zajcsökkentés
-    edged = cv2.Canny(bfilter, 10, 210) #Canny algoritmus 30,200, 10, 400
-    korvonalas_kep=cv2.cvtColor(edged, cv2.COLOR_BGR2RGB)
-    cv2.imshow("Korvonalazott kep", korvonalas_kep)
+def numberplate_to_text(img):    
+    #cv2.imshow("Rendszamtabla koruli kontur", img)
+    #cv2.waitKey(0)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow("Rendszamtabla koruli kontur", gray)
+    #cv2.waitKey(0)
+    bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
+    edged = cv2.Canny(bfilter, 100, 300) #Canny algoritmus 30,200, 10, 400
+    #cv2.imshow("Rendszamtabla koruli kontur", edged)
+    #cv2.waitKey(0)
 
 #---------------kontúrkeresés-----------------
     
     keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
     contours = imutils.grab_contours(keypoints)
-    contour_kiiratas =cv2.drawContours(img.copy(), contours, -1, (0,255,0),1)
-    cv2.imshow("Konturok", contour_kiiratas)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10] # rendezi és visszaadja az első 10-et
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
 #----------------rendszám megkeresése-----------------------
     location = None
-    for contour in contours: #ugye rendeztük korábban
-        approx = cv2.approxPolyDP(contour, 10, True) #approximate polygon from contour, közelíti a poligont a kontúrbol, minel nagyobb a középső argumentum, annál jobban kezeli egyenesnek a "recés részeket"
+    for contour in contours: 
+        approx = cv2.approxPolyDP(contour, 10, True)
         if len(approx) == 4:
-            location = approx #ha megvan a 4 sarokpont akkor elmenti és kilép
+            location = approx
             break
-
+    if (location is None):
+        new_text = "Rendszamot nem lehet detektalni"
+    
+    else:
 #---------maszk létrehozása és illesztése-------------------
-        
-    mask = np.zeros(gray.shape, np.uint8) #maszk
-    new_image = cv2.drawContours(mask, [location], 0,255, -1) # létrehozza az ideiglenes képet ami a korábban számított sarokpontok között van (location)
-    new_image2 = cv2.drawContours(img.copy(), [location],-1,(0,255,0),2)#kirajzoláshoz kell
-    cv2.imshow("Rendszamtabla koruli kontur", new_image2)
-    new_image = cv2.bitwise_and(img, img, mask=mask) # az ideiglenes képre amin a maszk van berakja az eredeti képet, így csak a rendszámtábla látszik 
+        mask = np.zeros(gray.shape, np.uint8)
+        new_image = cv2.drawContours(mask, [location], 0, 255, -1)
+        new_image2 = cv2.drawContours(img.copy(), [location],-1,(0,255,0),2)
+        new_image2 = cv2.resize(new_image2,None,fx = 0.6,fy=0.6)
+#         cv2.imshow("Rendszamtabla koruli kontur", new_image2)
+#         cv2.waitKey(0)
+        new_image = cv2.bitwise_and(img, img, mask=mask)
     
 #-----------cropping--------------------
-    
-    (x,y) = np.where(mask==255)
-    (x1, y1) = (np.min(x), np.min(y))
-    (x2, y2) = (np.max(x), np.max(y))
-    cropped = gray[x1:x2+1, y1:y2+1]
-    cv2.imshow("1. lépés", cropped)
+        (x,y) = np.where(mask==255)
+        (x1, y1) = (np.min(x), np.min(y))
+        (x2, y2) = (np.max(x), np.max(y))
+        cropped = gray[x1:x2+1, y1:y2+1]
+        
+        location = location.reshape(4,2)
+        pts = np.array(location, dtype="float32")
+        warped = four_point_transform(gray, location)
+        warped = cv2.bilateralFilter(warped, 11, 90, 90)             
+#         cv2.imshow("cropped_and_man",warped)
+#         cv2.waitKey(0)        
 
 #----------string kinyerése tesseract-------------
-    
-    text = pytesseract.image_to_string(cropped, config='--psm 11')
-    new_text = text.strip()
+        #binaris kep legyen
+        text = pytesseract.image_to_string(warped, config='--psm 11', lang='eng')
+        new_text = text.strip("")
+        new_text = ''.join(ch.upper() for ch in new_text if ch.isalnum())
+        cv2.destroyAllWindows()
+        return new_text
 
-#----------string kinyerése easyocr-------------
-    
-    #reader = easyocr.Reader(['en'])
-    #result = reader.readtext(cropped)
-    #text = result[0][-2]
-    
-    return new_text
-   
-inputpath = input('Adja meg a kép elérési útját! (formátum: tesztkepek/ .. .jpg)\n')
-img = cv2.imread(inputpath) #kép beolvasása fájlba
-number_plate_text = numberplate_to_text(img)
-print("A rendszam:",number_plate_text)
-cv2.waitKey()
-cv2.destroyAllWindows()
+def test_script_different_cars():
+    predicted_list = []
+    known_plates = []
+    statistic = []
+    with open('teszt.txt') as file:
+        lines = file.readlines()
+        for line in lines:
+            inputpath = line.rstrip()
+            img = cv2.imread(inputpath)
+            number_plate_text = numberplate_to_text(img)
+            actual_plate=get_knownplate(inputpath)
+            predicted_list.append(number_plate_text)
+            known_plates.append(actual_plate)
+            match = round((similar(actual_plate, number_plate_text)*100), 2)
+            statistic.append(match)
+            print("Exact plate number: ",actual_plate, "   Predicted plate number: ",number_plate_text,"   Match: ",match,"%",'\n')
+            
+    return statistic       
+
+def punto_test():
+    known_plate = "RPS503"
+    paths = []
+    predicted_list = []
+    statistic = []
+    for i in range(16):
+        temp_string = "punto/"
+        temp_string = temp_string+(str(i+1))
+        paths.append(temp_string+".jpg")
+    for element in paths:
+        img = cv2.imread(element)
+        number_plate_text = numberplate_to_text(img)
+        predicted_list.append(number_plate_text)
+        match = round((similar(known_plate, number_plate_text)*100), 2)
+        statistic.append(match)
+        print("Exact plate number: ",known_plate, "   Predicted plate number: ",number_plate_text,"   Match: ",match,"%",'\n')
+    return statistic
+
+statistic = test_script_different_cars()
+print("Percentage of 81 detected plates: ",round((sum(statistic)/81), 2), "%.\n\n")
+
+statistic_punto = punto_test()
+print("Percentage of 16 different angle: ",round((sum(statistic_punto)/16), 2), "%.\n\n")
